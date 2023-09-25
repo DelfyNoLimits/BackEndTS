7e6a3528bcb456bde621d4000664f60a6612125b
DelfyNoLimits <nickhardav@gmail.com>
2023-09-25 09:41:25 -0500

    online frontend

--- src/main/java/com/upc/edu/BackEndTripStore/BackEndTripStoreApplication.java
+++ src/main/java/com/upc/edu/BackEndTripStore/BackEndTripStoreApplication.java
@@ -7,12 +7,14 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @SpringBootApplication
 @EnableWebMvc
 public class BackEndTripStoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackEndTripStoreApplication.class, args);
+
 	}
 
 	@Bean
